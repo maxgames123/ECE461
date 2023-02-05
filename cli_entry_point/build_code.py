2bd96ea89f0416119e7fa1dc079f2d68787559f3
omartounsi7 <otounsi@purdue.edu>
2023-02-05 18:27:16 -0500

    updated build_code.py

--- cli_entry_point/build_code.py
+++ cli_entry_point/build_code.py
@@ -3,5 +3,4 @@ import os
 def build:
     os.system("cd ../repo_analyzer")
     os.system("cargo build")
-    os.system("cd ../repo_analyzer")
     os.system("mv /target/debug/filename.dll ../cli_entry_point")
