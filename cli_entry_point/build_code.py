1b41fc3c16d924c510f6eb83d917826bd99a44c9
omartounsi7 <otounsi@purdue.edu>
2023-02-05 20:27:07 -0500

    fixed typo in build()

--- cli_entry_point/build_code.py
+++ cli_entry_point/build_code.py
@@ -1,6 +1,8 @@
 import os
 
-def build:
-    os.system("cd ../repo_analyzer")
-    os.system("cargo build")
-    os.system("mv /target/debug/filename.dll ../cli_entry_point")
+def build():
+    os.system("cd ../repo_analyzer; cargo build")
+    print("Build functionality not finished yet!")
+
+    # Fix the line of code below!
+    # os.system("mv /target/debug/FILENAME_HERE!.dll ../cli_entry_point")
