cf5c59766581f210e8814981e5a5c6be19443ea0
Max <mmanzhos@purdue.edu>
2023-02-06 01:38:58 -0500

    Added some comments for readability

--- repo_analyzer/src/url_input.rs
+++ repo_analyzer/src/url_input.rs
@@ -5,15 +5,20 @@ pub fn run(url: &str) -> Box<String> {
 
 use regex::Regex;
 
+/// Returns true if there was a match to the regex
 pub fn is_valid(url: &str, regex: &Regex) -> bool {
     regex.is_match(url)
 }
 
+/// Parses a URL and returns its root domain and subdirectories
 pub fn get_data(url: &str) -> Vec<String> {
     let regular_expression = Regex::new(r"^https?://(www.)?(npmjs|github).com/(.+)").unwrap();
     if is_valid(url, &regular_expression) {
+        /// Returns all groups the regex captured in the URL
         let matches = regular_expression.captures(url).unwrap();
+        /// Root Domain (Group 2)
         let domain = &matches[2];
+        /// Subdirectories (Group 3)
         let subdirs = &matches[3];
         return vec![domain.to_string(), subdirs.to_string()]
     } else {
