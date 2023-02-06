edb9d612e282279c7669131a7ed810b375823d86
omartounsi7 <otounsi@purdue.edu>
2023-02-06 17:28:56 -0500

    implemented rampup metric

--- repo_analyzer/src/metric_calculations.rs
+++ repo_analyzer/src/metric_calculations.rs
@@ -15,10 +15,12 @@ pub fn get_responsiveness(url: &str) -> i32 {
     return 1;
 }
 
-pub fn get_ramp_up_time() {
-//     calculate ramp up time
-//     return score [0, 1]
+
+// The inputs to this function should be the number of lines in the module's documentation and codebase.
+pub fn get_ramp_up_time(docsLength: i32, codebaseLength: i32) -> f32 {
+    docsLength as f32 / codebaseLength as f32
 }
+
 
 pub fn get_bus_factor() {
 
