ad2b91de4f968fe947d91cc105b59974166f3ff3
omartounsi7 <otounsi@purdue.edu>
2023-02-06 20:46:50 -0500

    fixed the .dll issue in build

--- cli_entry_point/build_code.py
+++ cli_entry_point/build_code.py
@@ -1,6 +1,4 @@
 import os
 
-def build:
-    os.system("cd ../repo_analyzer")
-    os.system("cargo build")
-    os.system("mv /target/debug/filename.dll ../cli_entry_point")
+def build():
+    os.system("cd ../repo_analyzer/; cargo build; ls ; mv target/debug/librepo_analyzer.so ../cli_entry_point ; cd ../cli_entry_point ; ls")
