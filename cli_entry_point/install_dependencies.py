c87f95c325b08618219f7797c218bc67d1195689
omartounsi7 <otounsi@purdue.edu>
2023-02-06 20:34:54 -0500

    updated install

--- cli_entry_point/install_dependencies.py
+++ cli_entry_point/install_dependencies.py
@@ -3,4 +3,5 @@ import os
 def install():
     os.system("curl --proto '=https' -sSf https://sh.rustup.rs | sh") # installs rustup
     os.system("cargo install cargo-edit")
-    os.system("cd ../repo_analyzer; cargo add reqwest")
+    os.system("cd ../repo_analyzer; cargo add reqwest ; cargo add serde ; cargo add serde_json ; cargo add substring")
+    print("6 dependencies installed...")
