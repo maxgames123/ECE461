1d5331669075317062782276b740ead670350437
omartounsi7 <otounsi@purdue.edu>
2023-02-10 19:00:25 -0500

    added log dependencies installation

--- cli_entry_point/install_dependencies.py
+++ cli_entry_point/install_dependencies.py
@@ -3,5 +3,5 @@ import os
 def install():
     os.system("curl --proto '=https' -sSf https://sh.rustup.rs | sh") # installs rustup
     os.system("cargo install cargo-edit")
-    os.system("cd ../repo_analyzer; cargo add reqwest ; cargo add serde ; cargo add serde_json ; cargo add substring ; cargo add base64 ; cargo add async-recursion")
-    print("8 dependencies installed...")
+    os.system("cd ../repo_analyzer; cargo add reqwest ; cargo add serde ; cargo add serde_json ; cargo add substring ; cargo add base64 ; cargo add async-recursion ; cargo add log ; cargo add env_logger")
+    print("10 dependencies installed...")
