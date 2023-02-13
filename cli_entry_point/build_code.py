2f369a7b157ee9af0d111fdb8f8a51de3ccd98cb
omartounsi7 <62721212+omartounsi7@users.noreply.github.com>
2023-02-12 20:19:20 -0500

    Delete cli_entry_point directory

--- cli_entry_point/build_code.py
+++
@@ -1,4 +0,0 @@
-import os
-
-def build():
-    os.system("cd ../repo_analyzer/; cargo build")
--- cli_entry_point/install_dependencies.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def install():
-    os.system("curl --proto '=https' -sSf https://sh.rustup.rs | sh") # installs rustup
-    os.system("cargo install cargo-edit")
-    os.system("cd ../repo_analyzer; cargo add reqwest ; cargo add serde ; cargo add serde_json ; cargo add substring ; cargo add base64 ; cargo add async-recursion ; cargo add log ; cargo add env_logger")
-    print("10 dependencies installed...")
